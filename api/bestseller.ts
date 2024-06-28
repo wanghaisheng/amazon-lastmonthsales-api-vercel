f3263220a771c842d05c3f03316fb0ec8962538a
wanghaisheng <edwin_uestc@163.com>
2024-06-29 03:31:38 +0800

    add timeout to 60

--- api/bestseller.ts
+++ api/bestseller.ts
@@ -1,4 +1,4 @@
-export const maxDuration = 5; // This function can run for a maximum of 5 seconds
+export const maxDuration = 60; // This function can run for a maximum of 5 seconds
 export const dynamic = 'force-dynamic';
 import puppeteer from 'puppeteer'
 import { List } from './types'
--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-export const maxDuration = 5; // This function can run for a maximum of 5 seconds
+export const maxDuration = 60; // This function can run for a maximum of 5 seconds
 export const dynamic = 'force-dynamic';
 import { NextApiRequest, NextApiResponse } from "next"
 const { chromium: playwright } = require('playwright-core')
